26e72437d5cef67adee444885bf7eb3e3cea848e
vincentbwalker <vincethewalker@gmail.com>
2019-05-02 13:55:38 +0000

    Added task function and list

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 var app = new Vue({
     el: '#app',
     data: { 
+        tasks: [
+            { id: 1, name: 'Todo 1', description: 'This is Todo 1', completed: false },
+            { id: 2, name: 'Todo 2', description: 'This is Todo 2', completed: false },
+            { id: 3, name: 'Todo 3', description: 'This is Todo 3', completed: true },
+            { id: 4, name: 'Todo 4', description: 'This is Todo 1', completed: true }
+            ],
         message: 'Hello World!'
     }
 })
